2b7841709658aa6649a3c67670247802e96ac118
WojciechSz <w.szczygielski00@gmail.com>
2024-12-05 10:25:17 +0100

    Mobile nav, Nav links

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,21 +1,5 @@
-import { signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-
 const Home = async () => {
-  return (
-    <>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log Out</Button>
-      </form>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
 const Navbar = () => {
@@ -23,6 +24,7 @@ const Navbar = () => {
       <p>Global Search</p>
 
       <Theme />
+      <MobileNavigation />
     </nav>
   );
 };
