d537b7a738f12d612990b4f5a267415c71efa949
WojciechSz <w.szczygielski00@gmail.com>
2024-12-11 13:29:53 +0100

    Account Schema

---
+++ src/database/account.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema, models, Types } from "mongoose";
+
+export interface IAccount {
+  userId: Types.ObjectId;
+  name: string;
+  image?: string;
+  password?: string;
+  provider: string;
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema<IAccount>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    image: { type: String },
+    password: { type: String },
+    provider: { type: String, required: true },
+    providerAccountId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// Create a compound unique index on provider and providerAccountId
+AccountSchema.index({ provider: 1, providerAccountId: 1 }, { unique: true });
+
+const Account = models?.Account || model<IAccount>("Account", AccountSchema);
+
+export default Account;
--- src/database/user.model.ts
+++ src/database/user.model.ts
@@ -12,7 +12,7 @@ export interface IUser {
   reputation?: number;
 }
 
-const UserSchema = new Schema(
+const UserSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     username: { type: String, required: true },
