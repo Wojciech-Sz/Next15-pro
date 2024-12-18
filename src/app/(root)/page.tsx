158f095db3b4e478eea3a81240cfe17be721652e
WojciechSz <w.szczygielski00@gmail.com>
2024-12-18 08:09:17 +0100

    OAuth

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,12 @@
 import { SearchIcon } from "lucide-react";
 import Link from "next/link";
 
+import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import { api } from "@/lib/api";
-import handleError from "@/lib/handlers/error";
 
 const questions = [
   {
@@ -66,22 +65,15 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    return await api.users.getAll();
-  } catch (error) {
-    return handleError(error);
-  }
-};
-
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 const Home = async ({ searchParams }: SearchParams) => {
-  const users = await test();
-  console.log(users);
-
   const { query = "", filter = "" } = await searchParams;
+
+  const session = await auth();
+
+  console.log("Session", session);
 
   const filteredQuestions = questions.filter((question) => {
     const queryMatch = question.title
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,6 +3,7 @@ const ROUTES = {
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   ASK_QUESTION: "/ask-question",
+  SIGN_IN_WITH_OAUTH: "/signin-with-oauth",
   PROFILE: (id: string) => `/profile/${id}`,
   QUESTION: (id: string) => `/questions/${id}`,
   TAG: (id: string) => `/tags/${id}`,
