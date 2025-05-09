b2788b0fa7a87989e46bf927589e2d17cdc2db6a
WojciechSz <w.szczygielski00@gmail.com>
2025-05-09 19:08:39 +0200

    Vote fix

--- src/lib/actions/vote.action.ts
+++ src/lib/actions/vote.action.ts
@@ -61,7 +61,7 @@ export async function createVote(
   const { targetId, targetType, voteType } = validationResult.params!;
   const userId = validationResult?.session?.user?.id;
 
-  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
 
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -99,6 +99,15 @@ export async function createVote(
             targetType,
             voteType,
             change: 1,
+          },
+          session
+        );
+        await updateVoteCount(
+          {
+            targetId,
+            targetType,
+            voteType: existingVote.voteType,
+            change: -1,
           },
           session
         );
